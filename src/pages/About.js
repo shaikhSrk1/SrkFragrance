003c465b7018fda64288ef43e22128ac1b0e69d6
Shahrukh <srkfragrance@gmail.com>
2024-01-05 18:54:57 +0530

    modified about

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,10 +11,10 @@ const About = () => {
                         <img src={about} className="img-fluid rounded-circle" alt="about shop" />
                     </div>
                     <div className="col-lg-6">
-                        <h1 className="display-4 head">About Your Fragrance Haven</h1>
-                        <p>Welcome to Your Fragrance Haven, your one-stop destination for premium fragrances. Our passion for
+                        <h1 className="display-4 head">About Srk Fragrance </h1>
+                        <p>Welcome to Srk Fragrance , your one-stop destination for premium fragrances. Our passion for
                             scents and dedication to quality make us your ideal choice for all things perfume.</p>
-                        <p>At Your Fragrance Haven, we curate a wide range of scents from around the world. From designer
+                        <p> we curate a wide range of scents from around the world. From designer
                             perfumes to exotic fragrances, we have something for everyone. Our mission is to help you discover
                             the perfect scent that complements your style and personality.</p>
                     </div>
